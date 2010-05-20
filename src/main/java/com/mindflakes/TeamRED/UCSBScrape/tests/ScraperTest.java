5b36c420d14f437a9fc24d6eb5a3786f80cb77c4
Nelson Chen <crazysim@gmail.com>
2010-05-20 11:55:31 -0700

    added a scaffold test class for the scraper

---
+++ src/main/java/com/mindflakes/TeamRED/UCSBScrape/tests/ScraperTest.java
@@ -0,0 +1,24 @@
+package com.mindflakes.TeamRED.UCSBScrape.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ScraperTest {
+
+	@Test
+	public void testUCSBJMenuScraperStringBoolean() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUCSBJMenuScraperUCSBMenuFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMenus() {
+		fail("Not yet implemented");
+	}
+
+}
